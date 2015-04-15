86c5a8d348e8433a64ae507d823579f36d497028
Sorenfu <sh@compfix.dk>
2015-04-15 14:40:33 +0200

    Testing charts

--- app/js/app.js
+++ app/js/app.js
@@ -1647,6 +1647,20 @@ App.controller('ChartistController', ['$scope', function ($scope) {
         height: 260
     };
 
+    new Chartist.Line('.ct-chart', {
+        labels: weeklabels,
+        series: [
+            [12, 9, 7, 8, 5],
+            [2, 1, 3.5, 7, 3],
+            [1, 3, 4, 5, 6]
+        ]
+    }, {
+        fullWidth: true,
+        chartPadding: {
+            right: 40
+        }
+    });
+
 }]);
 
 /**=========================================================
